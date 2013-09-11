a5bae00bfbcf8931e778e7b0cc24eca65163067a
idsc3101 <idsc3101@gmail.com>
2013-09-11 17:23:56 -0500

    I solved it!!!!

--- problem1/BMICalc/BMICalc/Form1.vb
+++ problem1/BMICalc/BMICalc/Form1.vb
@@ -5,4 +5,31 @@ Public Class Form1
     Private Sub btnCalcBMI_Click(sender As System.Object, e As System.EventArgs) Handles btnCalcBMI.Click
 
     End Sub
+
+    Private Sub txtWeight_TextChanged(sender As System.Object, e As System.EventArgs) Handles txtWeight.TextChanged
+
+        Dim hst As String
+        Dim wst As String
+
+        hSt = txtHeight.Text
+        wst = txtWeight.Text
+
+        Dim h As Double
+        Dim w As Double
+
+        h = CDbl(hst)
+        w = CDbl(wst)
+
+        Dim BMI As Double
+
+        BMI = (w * 7.03) / (h ^ 2)
+
+        txtBodyMass.Text = CStr(BMI)
+
+
+
+
+
+
+    End Sub
 End Class
